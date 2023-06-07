4928b8da99c850cf7c69b6ed0e2180e8e8c4558d
valentino <v.arballo@itecriocuarto.org.ar>
2023-06-07 17:43:12 -0300

    progress_in_programs_page

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ def programs():
 
     file_name = "programs.json" # Nombre del archivo JSON
 
-    with open(file_name, "r") as json_file: # Abrir el archivo JSON
+    with open(file_name, "r", encoding="utf8") as json_file: # Abrir el archivo JSON, EL COLMO
         json_data = json.load(json_file)# Cargar el contenido del archivo JSON
 
     urls = []
     for country in json_data:
-        urls.append(country['main-img'])
+        urls.append(country['main_img'])
 
     return render_template('programs.html', countries=json_data, imagenes=urls) #le paso como parametro la data del json
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
