9248a64343a3b4f9c6102a5f0877746bc8e938f9
valentino <v.arballo@itecriocuarto.org.ar>
2023-06-11 21:03:44 -0300

    several changes in app.py and index.html files, initial mockup for inscriptions and programs files

--- app.py
+++ app.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+
+    file_name = "countries.json" # Nombre del archivo JSON
+    
+    with open(file_name, "r", encoding="utf8") as json_file: # Abrir el archivo JSON, EL COLMO
+        json_data = json.load(json_file)# Cargar el contenido del archivo JSON
+
+    urls = []
+    for country in json_data:
+        urls.append(country['landscape_img'])
+
+    return render_template('index.html', countries=json_data, imagenes=urls)
 
 @app.route('/programs')
 def programs():
@@ -20,6 +30,21 @@ def programs():
         urls.append(country['main_img'])
 
     return render_template('programs.html', countries=json_data, imagenes=urls) #le paso como parametro la data del json
+
+@app.route('/inscriptions')
+def inscriptions():
+
+    file_name = "countries.json" # Nombre del archivo JSON
+
+    with open(file_name, "r", encoding="utf8") as json_file: # Abrir el archivo JSON, EL COLMO
+        json_data = json.load(json_file)# Cargar el contenido del archivo JSON
+
+    urls = []
+    for country in json_data:
+        urls.append(country['main_img'])
+
+    return render_template('inscriptions.html', countries=json_data, imagenes=urls) #le paso como parametro la data del json
+
 
 if __name__ == '__main__':
     app.run()
