7c1956774a47a937216dcf17fb947fca08bc90e0
valentino <v.arballo@itecriocuarto.org.ar>
2023-06-18 20:38:42 -0300

    new navbar, countrys translated, optimized app

--- app.py
+++ app.py
@@ -3,45 +3,32 @@ import json
 
 app = Flask(__name__)
 
-@app.errorhandler(404)
+file_name = "countries.json" # Nombre del archivo JSON
 
+with open(file_name, "r", encoding="utf8") as json_file: # Abrir el archivo JSON, EL COLMO
+    json_data = json.load(json_file)# Cargar el contenido del archivo JSON
+
+urls_landscape_img = []
+urls_main_img = []
+
+for country in json_data:
+    urls_landscape_img.append(country['landscape_img'])
+    urls_main_img.append(country['main_img'])
+
+@app.errorhandler(404)
 def not_found(e):
     return render_template('404.html')
 
 @app.route('/')
 def index():
-
-    file_name = "countries.json" # Nombre del archivo JSON
-    
-    with open(file_name, "r", encoding="utf8") as json_file: # Abrir el archivo JSON, EL COLMO
-        json_data = json.load(json_file)# Cargar el contenido del archivo JSON
-
-    urls = []
-    for country in json_data:
-        urls.append(country['landscape_img'])
-
-    return render_template('index.html', countries=json_data, imagenes=urls)
+    return render_template('index.html', countries=json_data, imagenes=urls_landscape_img)
 
 @app.route('/programs')
 def programs():
-
-    file_name = "countries.json" # Nombre del archivo JSON
-
-    with open(file_name, "r", encoding="utf8") as json_file: # Abrir el archivo JSON, EL COLMO
-        json_data = json.load(json_file)# Cargar el contenido del archivo JSON
-
-    urls = []
-    for country in json_data:
-        urls.append(country['main_img'])
-
-    return render_template('programs.html', countries=json_data, imagenes=urls) #le paso como parametro la data del json
+    return render_template('programs.html', countries=json_data, imagenes=urls_main_img) #le paso como parametro la data del json
 
 @app.route('/country/<id>')
 def country(id):
-    file_name = "countries.json" # Nombre del archivo JSON
-
-    with open(file_name, "r", encoding="utf8") as json_file: 
-        json_data = json.load(json_file)
     try:
         id=int(id)
         country=json_data[id]
@@ -66,17 +53,7 @@ def country(id):
 
 @app.route('/inscriptions')
 def inscriptions():
-
-    file_name = "countries.json" # Nombre del archivo JSON
-
-    with open(file_name, "r", encoding="utf8") as json_file: # Abrir el archivo JSON, EL COLMO
-        json_data = json.load(json_file)# Cargar el contenido del archivo JSON
-
-    urls = []
-    for country in json_data:
-        urls.append(country['main_img'])
-
-    return render_template('inscriptions.html', countries=json_data, imagenes=urls) #le paso como parametro la data del json
+    return render_template('inscriptions.html', countries=json_data, imagenes=urls_main_img) #le paso como parametro la data del json
 
 @app.route('/about-us')
 def about_us():
